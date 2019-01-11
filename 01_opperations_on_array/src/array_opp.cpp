9e45902fa672ffdeff055c397215fe8f744aecf7
AdityaGhantasala <shine.aditya@gmail.com>
2019-01-11 11:18:14 +0100

    adding output in array_opp

--- 01_opperations_on_array/src/array_opp.cpp
+++ 01_opperations_on_array/src/array_opp.cpp
@@ -10,7 +10,12 @@
 
 RmaMPIOppTest::RmaMPIOppTest(const std::string FileName):mInputFileName(FileName)
 {
+    int proc_id, num_procs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
+    std::cout<<"From ArrayOpps :: this proc :: "<<proc_id<<std::endl;
+    std::cout<<"From ArrayOpps :: total proc :: "<<num_procs<<std::endl;
 }
 
 RmaMPIOppTest::~RmaMPIOppTest()
